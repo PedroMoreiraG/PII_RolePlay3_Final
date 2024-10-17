1ba805eb057f29253a5fe75629e95e5e866a3fa5
AgustinPalladino <agustin.palladino@correo.ucu.edu.uy>
2024-10-16 21:02:53 -0300

    Tests

--- test/LibraryTests/CharacterTests.cs
+++ test/LibraryTests/CharacterTests.cs
@@ -1,9 +1,15 @@
 using NUnit.Framework;
+using Ucu.Poo.RoleplayGame;
 
 namespace LibraryTests;
 
 [TestFixture]
 public class CharacterTests
 {
-    
+    [Test]
+    public void Prueba()
+    {
+        Dwarf dwarf = new Dwarf("Jorge");
+        Assert.That(100 == dwarf.Health);
+    }
 }
--- test/LibraryTests/ItemTests.cs
+++ test/LibraryTests/ItemTests.cs
@@ -1,9 +1,15 @@
 using NUnit.Framework;
+using Ucu.Poo.RoleplayGame;
 
 namespace LibraryTests;
 
 [TestFixture]
 public class ItemTests
 {
-    
+    [Test]
+    public void Prueba()
+    {
+        Archer archer = new Archer("Pedro");
+        Assert.That(15 == archer.AttackValue);
+    }
 }
